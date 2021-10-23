2f548f14119d097476af0a77a8ffdd5631ba6da2
gematro <71387349+gematro@users.noreply.github.com>
2021-10-23 18:36:14 +0300

    Add files via upload

--- calc/ciphers.js
+++ calc/ciphers.js
@@ -266,7 +266,7 @@ cipherList = [
 		false
 	),
 	new cipher(
-		"Anglossic Qabbala",
+		"Alphanumeric Qabbala",
 		"Thelemic",
 		60, 33, 62,
 		[97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122],
